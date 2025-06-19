5842dc66ee435870df8018aa47a2dce48bfd9ef8
Joan Chepkoech <joan92311@gmail.com>
2025-06-19 11:44:58 +0300

    Complete DOM modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Remove the <main id="main"> element from the DOM
+const main = document.getElementById('main');
+main.remove();
+
+// Create a new <h1> element and store it in a variable named newHeader
+const newHeader = document.createElement('h1');
+
+// Set the id of the newHeader element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of newHeader
+newHeader.textContent = 'Joan is the champion';
+
+// Append newHeader to the body of the document
+document.body.append(newHeader);
